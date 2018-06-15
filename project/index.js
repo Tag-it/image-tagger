3f9490efa2ff787aed940766835a472c4a05625c
ronnie1734 <rlewwwis@yahoo.com>
2018-06-14 20:27:19 -0700

    BUG: .env problem with 'dont know how to turn this value into a node'

--- project/index.js
+++ project/index.js
@@ -6,12 +6,16 @@ const superagent = require('superagent');
 const dotenv = require('dotenv');
 require('dotenv').config();
 
-
-
+const mongodb = require('mongodb');
+const db = require('./src/models/db');
+const mongoose = require('mongoose');
 const Bundler = require('parcel-bundler');
-let bundler = new Bundler('./public/index.html');
+let bundler = new Bundler('./index.html');
+
+mongoose.connect(process.env.MONGODB_URI);
+
 app.use(bundler.middleware());
 
-http.listen(3000, function () {
+http.listen(3000, function() {
     console.log('listening on PORT:3000')
 });
