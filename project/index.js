24469dbc58c200a357f2283ad31bd50436c95b38
michaeltreat <michael.leon.treat@gmail.com>
2018-06-16 16:17:06 -0700

    Fixed the issue with being able to use the array of files. index.js stores the files in masterData. There is still an issue with having to auth the user the first time. To auth the user the first time, open up quickstart.js and right before the last line of module.exports, add in getAuth(). Then, follow the usual instructions by following the url provided in the terminal window and then supply the code. After that, remove the line from quickstart, then run nodemon index.js. See me @michaeltreat if you have any questiosn.

--- project/index.js
+++ project/index.js
@@ -5,29 +5,37 @@ const http = require('http').Server(app);
 const superagent = require('superagent');
 require('dotenv').config();
 
-const listFiles = require('./src/quickstart.js');
+const getAuth = require('./src/quickstart.js');
+let masterData;
 
-console.log('listFiles', listFiles);
+// files variable will be a promise, but also has the data in it. This means you can use .then and .catch if you like after files. 
+
+// Otherwise, masterData will hold your files.
+let files = new Promise( (resolve, reject) =>{
+  return getAuth(resolve)
+})
+  .then(data => {
+    console.log('data from index.js files', data)
+    masterData = data;
+    return data;
+})
+
+// When you hit localhost:3000/api it will send you back the masterData
+
 app.get('/api', (req, res) => {
-  listFiles()
-  .then(files => {
-    res.send(files);
-  })
-  .catch(() => {
-    res.send([]);
-  });
+  res.send(masterData)
 });
 
 // const Bundler = require('parcel-bundler');
 // let bundler = new Bundler('./index.html');
 // app.use(bundler.middleware());
 
-const mongodb = require('mongodb');
-const db = require('./src/models/db');
-const mongoose = require('mongoose');
+// const mongodb = require('mongodb');
+// const db = require('./src/models/db');
+// const mongoose = require('mongoose');
 
-//mongoose.connect(process.env.MONGODB_URI);
-mongoose.connect("mongodb://localhost/tag-it");
+// //mongoose.connect(process.env.MONGODB_URI);
+// mongoose.connect("mongodb://localhost/tag-it");
 
 http.listen(3000, function() {
     console.log('listening on PORT:3000')
