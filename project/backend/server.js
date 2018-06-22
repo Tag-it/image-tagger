951bfb90a93e3c89f7085c1e1e08fd1673aa1ef8
scubaguypnw <scubaphoto@gmail.com>
2018-06-21 21:53:34 -0700

    working on front-end routes

--- project/backend/server.js
+++ project/backend/server.js
@@ -3,8 +3,8 @@
 const app = require ('express')();
 require('dotenv').config();
 
-const getAuth = require('./quickstart.js');
-let masterData;
+// const getAuth = require('./quickstart.js');
+// let masterData;
 
 // files variable will be a promise, but also has the data in it. This means you can use .then and .catch if you like after files. 
 
@@ -21,15 +21,10 @@ let files = new Promise( (resolve, reject) =>{
 
 // When you hit localhost:3000/api it will send you back the masterData
 
-app.get('/api/OAUTHRedirect', (req, res) => {
-  // res.send(<img src="https://drive.google.com/uc?export=view&id=1DG8usqKVEGbWe1ADLzcjFCx1Y6IoleR5" alt="">)
-  // res.end();
-  console.log(req.body);
-  console.log(req.header);
-});
+
 
 app.get('/api', (req, res) => {
-  res.send(masterData);
+  res.send('App running');
   //res.send(<img src="https://drive.google.com/uc?export=view&id=1SK5pEBsxogrwL9mwuBHPXk7RT017YQvT" alt="">)
   res.end;
 })
